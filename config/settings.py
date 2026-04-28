a780206dde57401c470b142168be68471e96214b
tarek <tarek@localhost.localdomain>
2026-04-28 11:48:50 +0000

    fix BASE_DIR error

--- config/settings.py
+++ config/settings.py
@@ -14,12 +14,12 @@ from pathlib import Path
 
 from decouple import config
 import os
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
